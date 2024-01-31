e27c7e91a6bdaae444d261fd2dc0c179acedd7f4
Guillaume BARRANCO <guillaume.barranco@sogilis.com>
2024-01-31 12:31:30 +0100

    Exo 11

--- src/test/java/org/snowcamp/university/springmodulith/ChartreuseShopApplicationTest.java
+++ src/test/java/org/snowcamp/university/springmodulith/ChartreuseShopApplicationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 public class ChartreuseShopApplicationTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(ChartreuseShopApplicationTest.class);
@@ -16,5 +17,16 @@ public class ChartreuseShopApplicationTest {
     @Test
     void output_modularity_verify(){
         ApplicationModules.of(ChartreuseShopApplication.class).verify();
+    }
+
+    @Test
+    void generateDocumentation() {
+        ApplicationModules modules = ApplicationModules.of(ChartreuseShopApplication.class);
+        Documenter.CanvasOptions canvasOptions = Documenter.CanvasOptions.defaults()
+                .revealInternals();
+        Documenter.DiagramOptions diagramOptions = Documenter.DiagramOptions.defaults()
+                .withStyle(Documenter.DiagramOptions.DiagramStyle.C4);
+        new Documenter(modules)
+                .writeDocumentation(diagramOptions, canvasOptions);
     }
 }
