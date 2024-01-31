9e26454d01b8dbdc638a4100e03698a99d9012d1
Guillaume BARRANCO <guillaume.barranco@sogilis.com>
2024-01-31 10:34:55 +0100

    Exo 5

---
+++ src/main/java/org/snowcamp/university/springmodulith/CustomApplicationModuleDetectionStrategy.java
@@ -0,0 +1,15 @@
+package org.snowcamp.university.springmodulith;
+
+import org.springframework.modulith.core.ApplicationModuleDetectionStrategy;
+import org.springframework.modulith.core.JavaPackage;
+
+import java.util.stream.Stream;
+
+import static java.util.function.Predicate.not;
+
+public class CustomApplicationModuleDetectionStrategy implements ApplicationModuleDetectionStrategy {
+    @Override
+    public Stream<JavaPackage> getModuleBasePackages(final JavaPackage basePackage) {
+        return basePackage.getDirectSubPackages().stream().filter(not(javaPackage -> javaPackage.getName().equals("org.snowcamp.university.springmodulith.common")));
+    }
+}
